2d128e784b034e2b6672274de10bc78f149b5dbb
L-alam <lalam@bu.edu>
2026-04-21 17:54:24 -0400

    hardcoded PONG response to client

--- app/main.py
+++ app/main.py
@@ -7,8 +7,14 @@ def main():
 
     # Uncomment the code below to pass the first stage
     #
+    
+    # TCP server that listens on port 6379. Just like redis
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    connection, client_address = server_socket.accept()
+    connection.send(bytes("+PONG\r\n", "utf-8"))
+
+
+    
 
 
 if __name__ == "__main__":
